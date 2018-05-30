e1b8475413644776438332768b4a651c59002dce
Baibhab Mondal <vbmandal98@gmail.com>
2018-05-30 20:38:34 +0530

    env added to git ignore

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,4 +4,5 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
+  api_key: '"AIzaSyCy7PDOjpnTzA9W4-8ygwm9cpIcs17ZalQ"'
 })
--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
+  api_key: '"AIzaSyCy7PDOjpnTzA9W4-8ygwm9cpIcs17ZalQ"'
 }
