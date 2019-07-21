b8de22dba3f51577f223ebce2bd4e7651a70bd5d
Baibhab Mondal <vbmandal98@gmail.com>
2019-07-21 11:52:15 +0530

    final try

--- src/components/User/profile.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1>
-            This is the Profile page!
-        </h1>
-    </div>
-</template>
--- src/router/auth-guard.js
+++
@@ -1,9 +0,0 @@
-import { store } from '../store/index'
-
-export default (to, from, next) => {
-  if (store.getters.users) {
-    next()
-  } else {
-    next('/signin')
-  }
-}
